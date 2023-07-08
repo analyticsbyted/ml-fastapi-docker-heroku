08a5c03c205d3d9daf1ce4053c40c13dae0b4d0f
Ted <dickeyted84@gmail.com>
2023-07-07 23:37:46 -0400

    added files and folders

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-venfrom fastapi import FastAPI
+from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
